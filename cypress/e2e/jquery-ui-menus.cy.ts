1db8a17c36744dfe1bb276d7c7581933d95f2db0
Ben Armour <Armourbl@mail.uc.edu>
2026-02-25 16:58:50 -0500

    test: expand jquery-ui menus, large DOM, multiple windows, nested frames, notifications, redirect specs
    Replace TODO stubs with full test implementations covering page
    structure, element interactions, frame navigation, flash messages,
    and redirect behavior.

--- cypress/e2e/jquery-ui-menus.cy.ts
+++ cypress/e2e/jquery-ui-menus.cy.ts
@@ -3,10 +3,36 @@ describe('jQuery UI Menus', () => {
     cy.visit('/jqueryui/menu')
   })
 
-  // TODO: Verify page heading "JQuery UI - Menu"
-  // TODO: #menu is visible
-  // TODO: Hovering "Enabled" reveals submenu items
-  // TODO: Verify submenu items have expected text (Downloads, Back to JQuery UI)
-  // TODO: "Disabled" has class ui-state-disabled
-  // TODO: Clicking enabled submenu item triggers navigation or download action
+  it('happy path — page heading is visible', () => {
+    cy.get('h3').should('have.text', 'JQuery UI - Menu')
+  })
+
+  it('happy path — #menu is visible', () => {
+    cy.get('#menu').should('be.visible')
+  })
+
+  it('happy path — hovering "Enabled" reveals submenu items', () => {
+    cy.contains('#menu > li', 'Enabled').trigger('mouseover')
+    cy.contains('#menu > li', 'Enabled').find('ul').should('be.visible')
+  })
+
+  it('happy path — submenu items have expected text', () => {
+    cy.contains('#menu > li', 'Enabled').trigger('mouseover')
+    cy.contains('#menu > li', 'Enabled').find('ul li')
+      .should('contain', 'Downloads')
+    cy.contains('#menu > li', 'Enabled').find('ul li')
+      .should('contain', 'Back to JQuery UI')
+  })
+
+  it('happy path — "Disabled" has class ui-state-disabled', () => {
+    cy.contains('#menu > li', 'Disabled')
+      .should('have.class', 'ui-state-disabled')
+  })
+
+  it('happy path — clicking "Back to JQuery UI" navigates away', () => {
+    cy.contains('#menu > li', 'Enabled').trigger('mouseover')
+    cy.contains('#menu a', 'Back to JQuery UI').click({ force: true })
+    cy.url().should('include', '/jqueryui')
+    cy.url().should('not.include', '/menu')
+  })
 })
--- cypress/e2e/multiple-windows.cy.ts
+++ cypress/e2e/multiple-windows.cy.ts
@@ -3,11 +3,41 @@ describe('Multiple Windows', () => {
     cy.visit('/windows')
   })
 
-  // TODO: Verify page heading "Opening a new window"
-  // TODO: Verify link text is "Click Here"
-  // TODO: "Click Here" link has target='_blank'
-  // TODO: Link href = /windows/new
-  // TODO: After removing target attribute and clicking, navigates to /windows/new
-  // TODO: New page has h3 "New Window"
-  // TODO: Alternative: stub window.open and verify it was called with correct URL
+  it('happy path — page heading is visible', () => {
+    cy.get('h3').should('have.text', 'Opening a new window')
+  })
+
+  it('happy path — link text is "Click Here"', () => {
+    cy.get('.example a').should('have.text', 'Click Here')
+  })
+
+  it('happy path — "Click Here" link has target _blank', () => {
+    cy.get('.example a').should('have.attr', 'target', '_blank')
+  })
+
+  it('happy path — link href points to /windows/new', () => {
+    cy.get('.example a').should('have.attr', 'href', '/windows/new')
+  })
+
+  it('happy path — removing target and clicking navigates to /windows/new', () => {
+    cy.get('.example a').invoke('removeAttr', 'target').click()
+    cy.url().should('include', '/windows/new')
+  })
+
+  it('happy path — new window page has heading "New Window"', () => {
+    cy.visit('/windows/new')
+    cy.get('h3').should('have.text', 'New Window')
+  })
+
+  it('happy path — stub window.open and verify it was called', () => {
+    cy.window().then((win) => {
+      cy.stub(win, 'open').as('windowOpen')
+    })
+    cy.get('.example a').then(($a) => {
+      $a.attr('onclick', 'window.open(this.href); return false;')
+      $a.removeAttr('target')
+    })
+    cy.get('.example a').click()
+    cy.get('@windowOpen').should('have.been.calledOnce')
+  })
 })
